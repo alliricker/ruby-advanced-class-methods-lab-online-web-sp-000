1a86a217c1ef3abbe2dc48fa46610f941186c76b
Allison Ricker <alliricker@gmail.com>
2020-10-09 17:50:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ end
 def self.new_by_name(name)
 song = self.new
 song.name = name
-song 
+song
 end
 
 def self.create_by_name(name)
@@ -28,6 +28,10 @@ def self.create_by_name(name)
   song.name = name
   @@all << song
   song
+end
+
+def self.find_by_name(name)
+  self.all.find{ |song| song.name == name }
 end
 
 end
