948168501f7b22a8f07f3543ad45f0b2d4535243
Allison Ricker <alliricker@gmail.com>
2020-10-09 18:29:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ def self.new_from_filename(name)
   song = self.new
   song.name = (name.split(" ")[1]
   .chomp(".mp3"))
-  song.artist_name = (name.split [0])
+  song.artist_name = (name.split(" ") [0])
   song
 end
 
