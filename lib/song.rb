b7ad4931010c11fdc1f8d09e31781d8fe3df1b9f
Allison Ricker <alliricker@gmail.com>
2020-10-09 17:48:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,13 @@ def self.create
   song
 end
 
-def self.new_by_name(name)
+def self.new_by_name
+song = self.new
+song.name = name
+song 
+end
+
+def self.create_by_name(name)
   song = self.new
   song.name = name
   @@all << song
